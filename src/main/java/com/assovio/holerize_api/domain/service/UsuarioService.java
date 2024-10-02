70dc391496004dea7bc59bf9eaa19c5b37060797
LaironOliveira <lairon@assovio.com.br>
2024-10-02 11:49:45 -0300

    feat: adicionado tipo de erro servico indisponivel e implementado Generic Service para usuario

--- src/main/java/com/assovio/holerize_api/domain/service/UsuarioService.java
+++ src/main/java/com/assovio/holerize_api/domain/service/UsuarioService.java
@@ -2,7 +2,6 @@ package com.assovio.holerize_api.domain.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +9,7 @@ import com.assovio.holerize_api.domain.dao.UsuarioDAO;
 import com.assovio.holerize_api.domain.model.Usuario;
 
 @Service
-public class UsuarioService {
-    
-    @Autowired
-    private UsuarioDAO dao;
-
-    public Usuario save(Usuario usuario){
-        return dao.save(usuario);
-    }
+public class UsuarioService extends GenericService<Usuario, UsuarioDAO, Long> {
 
     public Optional<UserDetails> getUserDetailsByLogin(String login){
         return dao.findFirstUserDetailsByLogin(login);
